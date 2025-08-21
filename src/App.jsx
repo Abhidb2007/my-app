7851029d4b4266db2d944f0f6671cfe33dbe6137
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-21 06:58:41 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css"
 function App(){
+  function focusOnInput(){
+    document.getElementById("text").focus()
+  }
   return <div>
-    signUp
+    signUp<br/>
     <input id="name" type="text"></input>
-    <button>Submit</button>
+    <input id="name" type="text"></input>
+    <button onClick={{focusOnInput}}>Submit</button>
   </div>
 }
 export default App  
