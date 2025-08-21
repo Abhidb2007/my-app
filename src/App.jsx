2d9eda4cf9be4baddb457bbe72ae6a6109d86ea5
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-21 06:49:03 +0530

    submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,9 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
-
-// Main App
-function App() {
-  return (
-    <BrowserRouter>
-      {/* Navigation */}
-      <nav>
-        <Link to="/">Allen</Link> |{" "}
-        <Link to="/sports-play-class-10">Class 10</Link> |{" "}
-        <Link to="/sports-play-class-11">Class 11</Link>
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Landing />} />
-          <Route path="sports-play-class-10" element={<Class10 />} />
-          <Route path="sports-play-class-11" element={<Class11 />} />
-          
-        </Route>
-      </Routes>
-
-      {/* Footer */}
-      <footer>Footer | Contact Us</footer>
-    </BrowserRouter>
-  );
+import "./App.css"
+function App(){
+  return <div>
+    signUp
+    <input id="name" type="text"></input>
+    <button>Submit</button>
+  </div>
 }
-
-// Layout Component
-function Layout() {
-  return (
-    <div style={{ height: "100vh", backgroundColor: "yellow" }}>
-      <Header />
-
-      <div style={{ height: "50vh", backgroundColor: "red" }}>
-        <Outlet />
-      </div>
-
-      <footer style={{ padding: "6px",background:"pink" }}>Footer inside Layout</footer>
-    </div>
-  );
-}
-
-// Header Component
-function Header() {
-  return (
-    <header style={{ padding: "30px", backgroundColor: "lightblue" }}>
-      <h2>Website Header</h2>
-    </header>
-  );
-}
-
-
-
-// Landing Page
-function Landing() {
-  return (
-    <div>
-      <h1>🏠 Home Page</h1>
-    </div>
-  );
-}
-
-// Class 10 Page
-function Class10() {
-  return <div>⚽ Sports Play for Class 10th</div>;
-}
-
-// Class 11 Page
-function Class11() {
-  const navigate = useNavigate();
-
-  function redirectUser() {
-    navigate("/");
-  }
-
-  return (
-    <div>
-      🏀 Sports Play for Class 11th
-      <br />
-      <button onClick={redirectUser}>Go back to landing page</button>
-    </div>
-  );
-}
-
-export default App;
+export default App  
