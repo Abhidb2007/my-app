f019a1f60e1f59f22a01034ca99dd7d672ac6b14
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-21 18:12:56 +0530

    count

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import {useState} from "react";
 import "./App.css"
 //a clock with start and stop button
 function App(){
-  const[currentCount, setCurrentCount]=useState(0);
+  const[currentCount, setCurrentCount]=useState(1);
+  const timeRef=useRef(null);
   function startClock(){
-    setInterval(function(){
-      setCurrentCount(count=count+1);
+    if(timeRef.current)return;
+    timeRef.current=setInterval(function(){
+      setCurrentCount(c=>c+1);
   
   
     },1000);
-  }  
+  } 
+  function stopClock() {
+    clearInterval(timerRef.current);
+    timerRef.current = null; // reset so start can work again
+  }
 
   return <div>
     {currentCount}
-    <button input id="text"></button>
-    <button onClick={startClock}>start</button>
+    
+    <button onClick={startClock}>Start</button>
+    <button onClick={stopClock}>Stop</button>
   </div>
 
 }
