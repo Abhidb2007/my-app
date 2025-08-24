6c33e441b4cb7b89f054022e5ca0f0ca554f6352
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-24 11:08:05 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 function App(){
   return(
     <div>
-      hi there
+      <PostComponent/>
     </div>
   )
 }
@@ -10,8 +10,14 @@ const style={width:200,backgroundColor:"pink",paddding:"50px",borderRadius:"5px 
 function PostComponent(){
   return(
     <div style={style}>
+      <img src={"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRys3EE_iaBjNvT1ybIeGvK3ofEU2PRXCb7SI8M2Q_tkGvXeSqAflEwlBKlhvHbTz9NiZsPEP8j_YMCXevva_zNGA"} style={{
+        width:100,
+        height:100,
+        borderRadius:40
+      }}/>
 
     </div>
   )
 
 }
+export default App;
