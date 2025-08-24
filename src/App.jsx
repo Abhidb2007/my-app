91d369c1113f0236451e734dd116b1efb0dd2f66
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-24 10:46:43 +0530

    post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
-import React, {createContext, useContext,useState} from "react"
-const CountContext = createContext();
-const CountUpdateContext = createContext();
-export function useCount(){
-  return useContext(CountContext);
-}
-export function useCountUpdate(){
-  return useContext(CountUpdateContext);
-}
-function CountProvider({children}){
-  const[count, setCount]=useState(0);
-  const increment=()=>setCount(prev=>prev+1);
-  return (
-    <CountContext.Provider value={count}>
-      <CountUpdateContext.Provider value={increment}>
-        {children}
-      </CountUpdateContext.Provider>
-    </CountContext.Provider>
-  );
-
-} 
-function Display() {
-  const count=useCount();
-  return <h1>Count:{count}</h1>;
-
-}
-function Button (){
-  const increment=useCountUpdate();
-  return <button onClick={increment}>increase</button>;
-
-}
-export default function App(){
+import "./App.css";
+function App(){
   return(
-    <CountProvider>
-      <Display/>
-        <Button/>
-    </CountProvider>
+    <div>
+      hi there
+    </div>
   )
+}
+const style={width:200,backgroundColor:"pink",paddding:"50px",borderRadius:"5px Solid Block",borderColor:"black"}
+function PostComponent(){
+  return(
+    <div style={style}>
+
+    </div>
+  )
+
 }
